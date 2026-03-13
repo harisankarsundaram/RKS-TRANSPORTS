2d469146889d6a817216f39ee8dd5fd71edd594b
Harisankar-Sunadaram <sharisankar789@gmail.com>
2026-03-13 10:27:51 +0530

    feat(mock-gps): add mock tracking routes

---
+++ backend/src/routes/mockTrackingRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const MockTrackingController = require('../controllers/mockTrackingController');
+
+const router = express.Router();
+
+router.get('/vehicles', MockTrackingController.listVehicles);
+router.get('/vehicle/:id/location', MockTrackingController.getVehicleLocation);
+router.get('/vehicle/:id/route', MockTrackingController.getVehicleRoute);
+
+router.post('/trip/start', MockTrackingController.startTrip);
+router.post('/trip/end', MockTrackingController.endTrip);
+
+router.get('/trip/:id/progress', MockTrackingController.getTripProgress);
+router.get('/trip/:id/distance', MockTrackingController.getTripDistance);
+router.get('/trip/:id/eta', MockTrackingController.getTripEta);
+router.get('/trip/:id/history', MockTrackingController.getTripHistory);
+router.get('/trip/:id/fuel', MockTrackingController.getTripFuel);
+
+module.exports = router;
